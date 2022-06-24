290e67e53d5febc824ae3f7dca293b6970095614
Rajalakshmi Ramasubramanian <rajal@amazon.com>
2022-06-23 18:43:44 -0700

    Adding a new mode Tproxy to do the redirection of traffic for ServiceConnect Plugin

--- plugins/ecs-serviceconnect/e2eTests/e2e_test.go
+++ plugins/ecs-serviceconnect/e2eTests/e2e_test.go
@@ -18,6 +18,7 @@ package e2e
 import (
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -33,15 +34,16 @@ import (
 	"github.com/coreos/go-iptables/iptables"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 )
 
 const (
-	nsName       = "ecs-sc-testNS"
-	containerID  = "contain-er"
-	ifName       = "test"
-	ingressChain = "ECS_SERVICE_CONNECT_INGRESS"
-	egressChain  = "ECS_SERVICE_CONNECT_EGRESS"
-	pluginName   = "ecs-serviceconnect"
+	nsName            = "ecs-sc-testNS"
+	containerID       = "contain-er"
+	ingressChain      = "ECS_SERVICE_CONNECT_INGRESS"
+	egressTproxyChain = "ECS_SERVICE_CONNECT_DIVERT"
+	pluginName        = "ecs-serviceconnect"
 )
 
 var (
@@ -61,16 +63,18 @@ func TestValid(t *testing.T) {
 		}
 	}(preserve)
 
-	for _, filename := range []string{
+	for _, filePath := range []string{
 		"valid_empty_ingress",
 		"valid_ingress_with_port_intercept_1",
 		"valid_ingress_with_port_intercept_2",
 		"valid_ingress_without_port_intercept",
+		"valid_tproxy_redirect_ip",
+		"valid_tproxy_redirect_port",
 		"valid_without_egress",
 		"valid_without_ingress",
 	} {
-		t.Run(filename, func(t *testing.T) {
-			testValid(t, filename)
+		t.Run(filePath, func(t *testing.T) {
+			testValid(t, filePath)
 		})
 	}
 }
@@ -94,16 +98,21 @@ func TestInvalid(t *testing.T) {
 		"invalid_egress_ipv6_cidr_1":            "invalid parameter: EgressConfig IPv6 CIDR Address",
 		"invalid_egress_ipv6_cidr_2":            "invalid parameter: EgressConfig IPv6 CIDR Address",
 		"invalid_egress_listener_port":          "invalid port [-1] specified",
-		"invalid_empty_egress":                  "invalid port [0] specified",
+		"invalid_egress_redirect_ip_1":          "invalid parameter: EgressConfig RedirectIP",
+		"invalid_egress_redirect_ip_2":          "missing required parameter: EgressConfig Redirect IP",
+		"invalid_egress_redirect_ip_3":          "missing required parameter: Egress ListenerPort",
+		"invalid_empty_egress":                  "exactly one of ListenerPort and RedirectIP must be specified in Egress",
 		"invalid_empty_egress_vip":              "missing required parameter: EgressConfig VIP CIDR",
 		"invalid_ingress_intercept_port":        "invalid port [-1] specified",
 		"invalid_ingress_listener_port":         "invalid port [80000] specified",
-		"invalid_missing_egress_listener_port":  "invalid port [0] specified",
+		"invalid_missing_egress_listener_port":  "exactly one of ListenerPort and RedirectIP must be specified in Egress",
 		"invalid_missing_egress_vip":            "missing required parameter: EgressConfig VIP",
 		"invalid_missing_ingress_egress":        "either IngressConfig or EgressConfig must be present",
 		"invalid_missing_ingress_listener_port": "invalid port [0] specified",
 		"invalid_v6_missing_egress_vip":         "missing required parameter: EgressConfig VIP CIDR",
 		"invalid_missing_ip":                    "both V4 and V6 cannot be disabled",
+		"invalid_missing_redirect_mode":         "invalid parameter: Egress RedirectMode",
+		"invalid_redirect_mode":                 "invalid parameter: Egress RedirectMode",
 	} {
 		t.Run(filename, func(t *testing.T) {
 			testInvalid(t, filename, errorMsg)
@@ -136,14 +145,29 @@ func testValid(t *testing.T, filename string) {
 	// Create a network namespace to mimic the container's network namespace.
 	targetNS, err := netns.NewNetNS(nsName)
 	require.NoError(t, err,
-		"Unable to create the network namespace that represents the network namespace of the container")
+		"Unable to create the network namespace for the container")
 	defer targetNS.Close()
+
+	err = targetNS.Run(func() error {
+		link, err := netlink.LinkByName("lo")
+		if err != nil {
+			t.Logf("Unable to get the loopback interface")
+			return err
+		}
+		// Bring the interface up.
+		if err := netlink.LinkSetUp(link); err != nil {
+			t.Logf("Unable to setup loopback interface")
+			return err
+		}
+		return nil
+	})
+	require.NoError(t, err, "Failed to setup interface")
 
 	// Construct args to invoke the CNI plugin with.
 	execInvokeArgs := &invoke.Args{
 		ContainerID: containerID,
 		NetNS:       targetNS.GetPath(),
-		IfName:      ifName,
+		IfName:      "test",
 		Path:        os.Getenv("CNI_PATH"),
 	}
 
@@ -202,10 +226,11 @@ func validateIPRulesAdded(t *testing.T, netConf *config.NetConfig) {
 }
 
 // validateIngressIptableRulesAdded validates the IP table rules that are added for handling ingress traffic.
-func validateIngressIptableRulesAdded(t *testing.T, iptable *iptables.IPTables, netConf *config.NetConfig, mustExist bool) {
+func validateIngressIptableRulesAdded(t *testing.T, iptable *iptables.IPTables,
+	netConf *config.NetConfig, mustExist bool) {
 	for redirectPort, listenerPort := range netConf.IngressListenerToInterceptPortMap {
-		exist, _ := iptable.Exists("nat", ingressChain, "-p", "tcp", "--dport", strconv.Itoa(listenerPort),
-			"-j", "REDIRECT", "--to-port", strconv.Itoa(redirectPort))
+		exist, _ := iptable.Exists("nat", ingressChain, "-p", "tcp", "--dport",
+			strconv.Itoa(listenerPort), "-j", "REDIRECT", "--to-port", strconv.Itoa(redirectPort))
 		require.Equal(t, mustExist, exist, "Mismatch of expected rules to redirect ingress ports")
 
 		exist, _ = iptable.Exists("nat", "PREROUTING", "-p", "tcp", "-m", "addrtype", "!",
@@ -226,10 +251,113 @@ func getCidr(proto iptables.Protocol, netConf *config.NetConfig) string {
 }
 
 // validateEgressIptableRulesAdded validates the IP table rules that are added for handling egress traffic.
-func validateEgressIptableRulesAdded(t *testing.T, iptable *iptables.IPTables, proto iptables.Protocol, netConf *config.NetConfig, mustExist bool) {
-	exist, _ := iptable.Exists("nat", "OUTPUT", "-p", "tcp", "-d", getCidr(proto, netConf),
-		"-j", "REDIRECT", "--to-port", strconv.Itoa(netConf.EgressPort))
-	require.Equal(t, mustExist, exist, "Mismatch in expected rules to redirect ports")
+func validateEgressIptableRulesAdded(t *testing.T, iptable *iptables.IPTables, proto iptables.Protocol,
+	netConf *config.NetConfig, mustExist bool) {
+	switch netConf.EgressRedirectMode {
+	case config.NAT:
+		exist, _ := iptable.Exists("nat", "OUTPUT", "-p", "tcp", "-d", getCidr(proto, netConf),
+			"-j", "REDIRECT", "--to-port", strconv.Itoa(netConf.EgressPort))
+		require.Equal(t, mustExist, exist, "Mismatch in expected rules to redirect ports")
+	case config.TPROXY:
+		if netConf.EgressRedirectIPv4Addr != "" || netConf.EgressRedirectIPv6Addr != "" {
+			verifyEgressRedirectIP(t, proto, netConf, true)
+		} else {
+			verifyEgressTproxyRules(t, iptable, proto, netConf, true)
+		}
+	}
+}
+
+// verifyEgressRedirectIP validates the rules set in the case of Tproxy Redirection IP
+func verifyEgressRedirectIP(t *testing.T, proto iptables.Protocol, netConf *config.NetConfig, mustExist bool) {
+	handle, err := netlink.NewHandle()
+	require.NoError(t, err, "Failed to get new handle")
+
+	var dst *net.IPNet
+	var redirectionIP net.IP
+	if proto == iptables.ProtocolIPv6 {
+		_, dst, _ = net.ParseCIDR(netConf.EgressIPv6CIDR)
+		redirectionIP = net.ParseIP(netConf.EgressRedirectIPv6Addr)
+	} else {
+		_, dst, _ = net.ParseCIDR(netConf.EgressIPv4CIDR)
+		redirectionIP = net.ParseIP(netConf.EgressRedirectIPv4Addr)
+	}
+
+	route := &netlink.Route{
+		Dst: dst,
+		Gw:  redirectionIP,
+	}
+	family := getFamily(proto)
+	_, err = handle.RouteListFiltered(family, route, netlink.RT_FILTER_GW|netlink.RT_FILTER_DST)
+	require.NoError(t, err, "Failed to list routes")
+}
+
+// verifyEgressTproxyRules validates the presence or absence of all the rules needed for Tproxy redirection.
+func verifyEgressTproxyRules(t *testing.T, iptable *iptables.IPTables, proto iptables.Protocol,
+	netConf *config.NetConfig, mustExist bool) {
+	exist, _ := iptable.Exists("mangle", egressTproxyChain, "-j", "MARK", "--set-mark", "1")
+	require.Equal(t, mustExist, exist, "Mismatch in expected rules for tproxy mark")
+	exist, _ = iptable.Exists("mangle", egressTproxyChain, "-j", "ACCEPT")
+	require.Equal(t, mustExist, exist, "Mismatch in expected rules for tproxy chain")
+	exist, _ = iptable.Exists("mangle", "PREROUTING", "-p", "tcp", "-m",
+		"socket", "-j", egressTproxyChain)
+	require.Equal(t, mustExist, exist, "Mismatch in expected rules for prerouting chain")
+	exist, _ = iptable.Exists("mangle", "PREROUTING", "-p", "tcp",
+		"-m", "tcp", "-d", getCidr(proto, netConf), "-j", "TPROXY", "--tproxy-mark", "0x1/0x1", "--on-port",
+		strconv.Itoa(netConf.EgressPort))
+	require.Equal(t, mustExist, exist, "Mismatch in expected rules for prerouting chain")
+	handle, err := netlink.NewHandle()
+	require.NoError(t, err, "Failed to get new handle")
+	family := getFamily(proto)
+	allRules, err := handle.RuleList(family)
+	require.NoError(t, err, "Failed to list all ip rules")
+	rule := netlink.NewRule()
+	rule.Family = family
+	rule.Mark = 1
+	rule.Table = 100
+	require.Equal(t, mustExist, ruleExists(t, allRules, rule), "Mismatch in expected rules")
+	link, err := netlink.LinkByName("lo")
+	require.NoError(t, err, "Failed to get loopback interface")
+
+	route := &netlink.Route{
+		Table:     100,
+		LinkIndex: link.Attrs().Index,
+	}
+	routes, err := handle.RouteListFiltered(family, route, netlink.RT_FILTER_TABLE|netlink.RT_FILTER_OIF)
+	require.Equal(t, 1, len(routes))
+}
+
+// getFamily returns the family given the protocol.
+func getFamily(proto iptables.Protocol) int {
+	if proto == iptables.ProtocolIPv6 {
+		return unix.AF_INET6
+	} else {
+		return unix.AF_INET
+	}
+}
+
+// ruleExists verifies whether the given rule exists in the given list of rules.
+func ruleExists(t *testing.T, rules []netlink.Rule, rule *netlink.Rule) bool {
+	for i := range rules {
+		if ruleEquals(t, &rules[i], rule) {
+			return true
+		}
+	}
+	return false
+}
+
+// ruleEquals returns whether the given rules can be considered equal.
+func ruleEquals(t *testing.T, a, b *netlink.Rule) bool {
+	return a.Table == b.Table &&
+		((a.Src == nil && b.Src == nil) ||
+			(a.Src != nil && b.Src != nil && a.Src.String() == b.Src.String())) &&
+		((a.Dst == nil && b.Dst == nil) ||
+			(a.Dst != nil && b.Dst != nil && a.Dst.String() == b.Dst.String())) &&
+		a.OifName == b.OifName &&
+		a.IifName == b.IifName &&
+		a.Invert == b.Invert &&
+		a.Mark == b.Mark &&
+		a.TunID == b.TunID &&
+		a.Goto == b.Goto
 }
 
 // getProto returns map of protocol and whether it needs to be handled.
@@ -259,8 +387,8 @@ func validateIPRulesDeleted(t *testing.T, netConf *config.NetConfig) {
 // validateIngressIptableRulesDeleted validates that the ingress IP rules do not exist.
 func validateIngressIptableRulesDeleted(t *testing.T, iptable *iptables.IPTables, netConf *config.NetConfig) {
 	for redirectPort, listenerPort := range netConf.IngressListenerToInterceptPortMap {
-		exist, _ := iptable.Exists("nat", ingressChain, "-p", "tcp", "--dport", strconv.Itoa(listenerPort),
-			"-j", "REDIRECT", "--to-port", strconv.Itoa(redirectPort))
+		exist, _ := iptable.Exists("nat", ingressChain, "-p", "tcp", "--dport",
+			strconv.Itoa(listenerPort), "-j", "REDIRECT", "--to-port", strconv.Itoa(redirectPort))
 		require.False(t, exist, "Found unexpected rules to redirect ingress ports")
 
 		exist, _ = iptable.Exists("nat", "PREROUTING", "-p", "tcp", "-m", "addrtype", "!",
@@ -270,11 +398,20 @@ func validateIngressIptableRulesDeleted(t *testing.T, iptable *iptables.IPTables
 }
 
 // validateEgressIptableRulesDeleted validates that the egress IP rules do not exist.
-func validateEgressIptableRulesDeleted(t *testing.T, iptable *iptables.IPTables, proto iptables.Protocol, netConf *config.NetConfig) {
-
-	exist, _ := iptable.Exists("nat", "OUTPUT", "-p", "tcp", "-d", getCidr(proto, netConf),
-		"-j", "REDIRECT", "--to-port", strconv.Itoa(netConf.EgressPort))
-	require.False(t, exist, "Found unexpected rules to redirect cidr")
+func validateEgressIptableRulesDeleted(t *testing.T, iptable *iptables.IPTables, proto iptables.Protocol,
+	netConf *config.NetConfig) {
+	switch netConf.EgressRedirectMode {
+	case config.NAT:
+		exist, _ := iptable.Exists("nat", "OUTPUT", "-p", "tcp", "-d", getCidr(proto, netConf),
+			"-j", "REDIRECT", "--to-port", strconv.Itoa(netConf.EgressPort))
+		require.False(t, exist, "Found unexpected rules to redirect cidr")
+	case config.TPROXY:
+		if netConf.EgressRedirectIPv4Addr != "" || netConf.EgressRedirectIPv6Addr != "" {
+			verifyEgressRedirectIP(t, proto, netConf, false)
+		} else {
+			verifyEgressTproxyRules(t, iptable, proto, netConf, false)
+		}
+	}
 }
 
 // testInvalid tests whether the test file returns the given error.
@@ -293,7 +430,7 @@ func testInvalid(t *testing.T,
 	execInvokeArgs := &invoke.Args{
 		ContainerID: containerID,
 		NetNS:       targetNS.GetPath(),
-		IfName:      ifName,
+		IfName:      "test",
 		Path:        os.Getenv("CNI_PATH"),
 	}
 
