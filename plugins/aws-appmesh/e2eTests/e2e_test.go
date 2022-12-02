ab2fabdca0a660a6479175fb355a3882104e2ee3
Edward Xiao <edwardxf@amazon.com>
2022-12-02 00:45:34 +0000

    fix tag

--- plugins/aws-appmesh/e2eTests/e2e_test.go
+++ plugins/aws-appmesh/e2eTests/e2e_test.go
@@ -11,8 +11,8 @@
 // express or implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
-//go:build e2e-test
-// +build e2e-test
+//go:build e2e_test
+// +build e2e_test
 
 package e2e
 
